72ecde6c32e4d679947f8d6a6e21281388149f43
gaogeek <326860002@qq.com>
2017-08-07 21:29:30 +0800

    优化精简代码

--- toast/src/main/java/com/gaogeek/toast/Toast.java
+++ toast/src/main/java/com/gaogeek/toast/Toast.java
@@ -3,23 +3,17 @@ package com.gaogeek.toast;
 /**
  * Created by gaogeek on 2017/8/2.
  */
-import android.content.Context;
-import android.os.Build;
-import android.os.Handler;
 
+import android.content.Context;
+import android.os.Handler;
 import android.os.Message;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
-import java.util.Timer;
-import java.util.TimerTask;
 
 public class Toast {
 
-    private static int currentapiVersion = Build.VERSION.SDK_INT;
-
     private Handler handler;
-    private static Timer timer;
 
     public static int LENGTH_SHORT = android.widget.Toast.LENGTH_SHORT;
     public static int LENGTH_LONG = android.widget.Toast.LENGTH_LONG;
@@ -52,10 +46,8 @@ public class Toast {
         }
         this.mContext = context;
         initTN();
-        if (instance != null)
-            throw new NullPointerException("error");
         if (handler == null) {
-            handler = new Handler(){
+            handler = new Handler() {
                 @Override
                 public void handleMessage(Message msg) {
                     Toast.this.hideToast();
@@ -73,9 +65,9 @@ public class Toast {
     }
 
     public void setDuration(int t) {
-        if(t == android.widget.Toast.LENGTH_SHORT) {
-            this.time = 2000;
-        } else if(t == android.widget.Toast.LENGTH_LONG) {
+        if (t == android.widget.Toast.LENGTH_SHORT) {
+            this.time = 2500;
+        } else if (t == android.widget.Toast.LENGTH_LONG) {
             this.time = 3500;
         } else if (t > 1000) {
             this.time = t;
@@ -83,26 +75,12 @@ public class Toast {
     }
 
     public final void show() {
-        if (hasReflectException) {
-            android.widget.Toast t = new android.widget.Toast(mContext);
-            t.setText(text);
-            t.setDuration(android.widget.Toast.LENGTH_SHORT);
-            t.show();
-            initTN();
-            return;
+        if (!hasReflectException) {
+            mToast.setText(text);
+            showToast();
+            handler.removeMessages(1);
+            handler.sendEmptyMessageDelayed(1, time);
         }
-        mToast.setText(text);
-        showToast();
-        if (timer != null) {
-            timer.cancel();
-        }
-        timer = new Timer();
-        timer.schedule(new TimerTask() {
-            @Override
-            public void run() {
-                handler.sendEmptyMessage(1);
-            }
-        }, time);
     }
 
     private void initTN() {
@@ -115,53 +93,35 @@ public class Toast {
             mTN = clazz.getDeclaredField("mTN");
             mTN.setAccessible(true);
             mObj = mTN.get(mToast);
-            showMethod = mObj.getClass().getDeclaredMethod("show",
-                    new Class<?>[0]);
-            hideMethod = mObj.getClass().getDeclaredMethod("hide",
-                    new Class<?>[0]);
+            showMethod = mObj.getClass().getDeclaredMethod("show", new Class<?>[0]);
+            hideMethod = mObj.getClass().getDeclaredMethod("hide", new Class<?>[0]);
             Field mY = mObj.getClass().getDeclaredField("mY");
             mY.setAccessible(true);
             mY.set(mObj, dip2px(mContext, 68F));
             hasReflectException = false;
-        } catch (NoSuchFieldException e) {
+          } catch (Exception e) {
             hasReflectException = true;
-            System.out.println(e.getMessage());
-        } catch (IllegalAccessException e) {
-            hasReflectException = true;
-            System.out.println(e.getMessage());
-        } catch (IllegalArgumentException e) {
-            hasReflectException = true;
-            System.out.println(e.getMessage());
-        } catch (NoSuchMethodException e) {
-            hasReflectException = true;
-            System.out.println(e.getMessage());
         }
     }
 
     private void showToast() {
         try {
-            if (currentapiVersion > 10) {
-                Field mNextView = mObj.getClass().getDeclaredField("mNextView");
-                mNextView.setAccessible(true);
-                mNextView.set(mObj, mToast.getView());
-            }
+            Field mNextView = mObj.getClass().getDeclaredField("mNextView");
+            mNextView.setAccessible(true);
+            mNextView.set(mObj, mToast.getView());
             showMethod.invoke(mObj, new Object[0]);
             hasReflectException = false;
         } catch (Exception e) {
             hasReflectException = true;
-            System.out.println(e.getMessage());
         }
     }
 
     private void hideToast() {
-        timer.cancel();
-        timer = null;
         try {
             hideMethod.invoke(mObj, new Object[0]);
             hasReflectException = false;
         } catch (Exception e) {
             hasReflectException = true;
-            System.out.println(e.getMessage());
         }
     }
 
